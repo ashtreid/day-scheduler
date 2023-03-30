ad5718d8794b7334e8f98666355b6df44c3c26cf
Ash Reid <ashley.reid013@gmail.com>
2023-03-29 21:38:17 -0600

    adds clear storage and removes extra comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function() {
   function headingTodaysDate() {
     const todaysDate = dayjs().format("dddd | MMMM D, YYYY");
     $('#currentDay').text(todaysDate).css("fontSize", "30px");
   };
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
   function storeValsOnSave() {
     $('.saveBtn').on('click', function(event) {
@@ -36,13 +27,6 @@ $(document).ready(function() {
     });
   };
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   function addTimeBlocks() {
     for (var i = 1; i <= 23; i++) {
       var currentHour = dayjs().hour();
@@ -64,14 +48,15 @@ $(document).ready(function() {
     };
   };
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  function clearScheduleLocal() {
+    $('.btn-danger').on('click', function() {
+      localStorage.clear();
+      location.reload();
+    });
+  };
 
   headingTodaysDate();
   addTimeBlocks();
   storeValsOnSave();
+  clearScheduleLocal();
 });
